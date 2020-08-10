edb41ef019a3221e21ae440c1b587cd9ceb5a909
vortesnail <1091331061@qq.com>
2020-08-11 01:18:48 +0800

    chore: setup proxy

--- scripts/config/webpack.dev.js
+++ scripts/config/webpack.dev.js
@@ -3,6 +3,8 @@ const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 const { SERVER_HOST, SERVER_PORT } = require('../constants')
 
+const proxySetting = require('../../src/setProxy')
+
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval-source-map',
@@ -14,6 +16,7 @@ module.exports = merge(common, {
     compress: true, // 是否启用 gzip 压缩
     open: true, // 打开默认浏览器
     hot: true, // 热更新
+    proxy: { ...proxySetting }
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
